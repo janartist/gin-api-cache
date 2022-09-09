646a574d5c5eaefa61e38bb3a13e2d2625359c98
Jan <jan@janMacBookPro.local>
2022-09-09 16:22:30 +0800

    add benchMarke

--- example/route_test.go
+++ example/route_test.go
@@ -6,15 +6,43 @@ import (
 	"testing"
 )
 
+var engine *gin.Engine
+
+func init() {
+	engine = RunWithRedis()
+}
+
 func performRequest(method, target string, router *gin.Engine) *httptest.ResponseRecorder {
 	r := httptest.NewRequest(method, target, nil)
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, r)
 	return w
 }
-
+func BenchmarkNoCache(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		resp := performRequest("GET", "/test", engine)
+		if resp.Body.String() != "test-res" {
+			b.Errorf("[/test] err is %s", resp.Body.String())
+		}
+	}
+}
+func BenchmarkCache(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		resp := performRequest("GET", "/test-cache-second", engine)
+		if resp.Body.String() != "test-cache-second-res" {
+			b.Errorf("[/test] err is %s", resp.Body.String())
+		}
+	}
+}
+func BenchmarkCacheWithSingle(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		resp := performRequest("GET", "/test-cache-second-single", engine)
+		if resp.Body.String() != "test-cache-second-single-res" {
+			b.Errorf("[/test] err is %s", resp.Body.String())
+		}
+	}
+}
 func TestCacheWithRedis(t *testing.T) {
-	r := RunWithRedis()
 	//r.Run()
 	outp := make(chan string, 50)
 	outpCache := make(chan string, 50)
@@ -22,17 +50,17 @@ func TestCacheWithRedis(t *testing.T) {
 	for i := 0; i < 50; i++ {
 		//default
 		go func() {
-			resp := performRequest("GET", "/test", r)
+			resp := performRequest("GET", "/test", engine)
 			outp <- resp.Body.String()
 		}()
 		//with cache
 		go func() {
-			resp := performRequest("GET", "/test-cache-second", r)
+			resp := performRequest("GET", "/test-cache-second", engine)
 			outpCache <- resp.Body.String()
 		}()
 		//with cache single
 		go func() {
-			resp := performRequest("GET", "/test-cache-second-single", r)
+			resp := performRequest("GET", "/test-cache-second-single", engine)
 			outpCacheSingle <- resp.Body.String()
 		}()
 	}
